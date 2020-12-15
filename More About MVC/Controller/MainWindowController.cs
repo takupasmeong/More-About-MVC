e51694d4c47e088dc9b83701d02182b2f8fb42cb
takupasmeong <abiyu.aronggear@students.amikom.ac.id>
2020-12-15 21:55:30 +0700

    Add project files.

---
+++ More About MVC/Controller/MainWindowController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using More_About_MVC.Model;
+
+namespace More_About_MVC.Controller
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+    }
+}
---
+++ More About MVC/Controller/PenawaranController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using More_About_MVC.Model;
+
+namespace More_About_MVC.Controller
+{
+    class PenawaranController
+    {
+        private List<Item> items;
+
+        public PenawaranController()
+        {
+            items = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.items;
+        }
+    }
+}
